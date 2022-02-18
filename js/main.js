47b0879e2ddd4f42235be364100ca11ceecd953a
Pavel <pavel_chermyanin@mail.ru>
2022-02-18 20:51:16 +0300

    12 lesson, created personalMovieDB, wrote down answers from prompt in property movies

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
-let number = 5;
-const obj = {
-    a: 60
-};
-obj.a = 70
-console.log('str' * 9);
+let numberOfFilms  = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms ,
+    movies: {},
+    acters: {},
+    genres: [],
+    privat: false
+}
+
+
+let witchMovie = prompt('Один из последних просмотренных фильмов?', '');
+let witchRate = +prompt('На сколько оцените его?', '');
+let witchMovie2 = prompt('Один из последних просмотренных фильмов?', '');
+let witchRate2 = +prompt('На сколько оцените его?', '');
+personalMovieDB.movies[witchMovie] = witchRate;
+personalMovieDB.movies[witchMovie2] = witchRate2;
+console.log(personalMovieDB)
