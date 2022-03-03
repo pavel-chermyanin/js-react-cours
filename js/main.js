50536a0861c48879760cbba55a36a0d226208f67
Pavel <pavel_chermyanin@mail.ru>
2022-03-03 20:52:07 +0300

    created slider

--- js/main.js
+++ js/main.js
@@ -1,6 +1,37 @@
 'use strict';
 
 
+    // function showSlides(n) {
+    //     if (n > slides.length) {
+    //         slideIndex = 1;
+    //     }
+    //     if(n < 1) {
+    //         slideIndex = slides.length
+    //     }
+
+    //     slides.forEach(item => item.style.display = 'none');
+
+    //     slides[slideIndex - 1].style.display = 'block';
+
+    //     if (slides.length < 10) {
+    //         current.textContent = `0${slideIndex}`;
+    //     } else {
+    //         current.textContent = slideIndex;
+    //     }
+    // }
+
+    // function plusSlides(n) {
+    //     showSlides(slideIndex += n);
+    // }
+
+    // prev.addEventListener('click', () => {
+    //     plusSlides(-1);
+    // })
+    // next.addEventListener('click', () => {
+    //     plusSlides(1);
+    // })
+
+
 // reduce
 
 // const arr = [4, 5, 1];
